87a30f610e359a4cd55337fc96d01262bb5004aa
Đoàn Thanh Phương <phuongdt@intern.ominext.com>
2022-06-28 22:53:10 +0700

    Part 2

--- app/src/main/java/com/example/mymemorygame/MainActivity.kt
+++ app/src/main/java/com/example/mymemorygame/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mymemorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rcvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rcvBoard.setHasFixedSize(true)
+        rcvBoard.layoutManager = GridLayoutManager(this, 2)
     }
 }
---
+++ app/src/main/java/com/example/mymemorygame/MemoryBoardAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.mymemorygame
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.memory_card.view.*
+import kotlin.math.min
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.cardView.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = numPieces
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val  imageButton = itemView.imgButton
+
+        fun bind(position: Int) {
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Click  $position")
+            }
+        }
+    }
+}
+
